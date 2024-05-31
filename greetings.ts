5ebb3de8da78cd1fb9cc86ed0264f32c9b16913d
Shubham Maske <shubham.maske@coradius.io>
2024-05-31 15:25:33 +0530

    basic of types in typescript

---
+++ greetings.ts
@@ -0,0 +1,11 @@
+let username:string = "Shubham"
+
+console.log(`Hello ${username}`)
+
+
+
+// if we assign a value immediately after declaring variable then
+// typescript automatically detect it a type of that value.
+let userId = 34
+
+export {}
