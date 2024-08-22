0280783377165adc850ead436bbe01d57fdc2a48
Shubham Maske <shubhammaske099@gmail.com>
2024-08-22 12:09:22 +0530

    added tsconfig.json file

--- greetings.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var username = "Shubham";
-console.log("Hello ".concat(username));
-// if we assign a value immediately after declaring variable then
-// typescript automatically detect it a type of that value.
-var userId = 34;
